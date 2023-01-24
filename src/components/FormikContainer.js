5c78df307c368a5ed7bbca3af971c2f0d1e38812
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-24 13:00:09 +0330

    co7

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -1,31 +1,69 @@
-import React from 'react'
-import {Formik, Form} from 'formik'
-import * as yup from 'yup'
-import FormikControl from './FormikControl'
+import React from "react";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import FormikControl from "./FormikControl";
 
 const FormikContainer = () => {
-    const initialValues = {
-        email:'',
-        description: ''
-    }
-    const validationSchema = yup.object({
-        email: yup.string().required('required'),
-        description: yup.string().required('required'),
-    })
-    const onSubmit = values => console.log('Form data', values)
+  const dropDownOptions = [
+    { key: "select an option", value: "" },
+    { key: "option 1", value: "option1" },
+    { key: "option 2", value: "option2" },
+    { key: "option 3", value: "option3" },
+  ];
+  const radioOptions = [
+    { key: "option 1", value: "option1" },
+    { key: "option 2", value: "option2" },
+    { key: "option 3", value: "option3" },
+  ];
+  const initialValues = {
+    email: "",
+    description: "",
+    selectOption: '',
+    radioOption: ''
+  };
+  const validationSchema = yup.object({
+    email: yup.string().required("required"),
+    description: yup.string().required("required"),
+    selectOption: yup.string().required("required"),
+    radioOption: yup.string().required("required"),
+  });
+  const onSubmit = (values) => console.log("Form data", values);
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
-        {
-            formik =>(
-                <Form>
-                    <FormikControl control='input' type='email' label='Email' name='email' />
-                    <FormikControl control='textarea' label='Description' name='description' />
-                    <button type='submit'>Submit</button>
-                </Form>
-            )
-        }
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      {(formik) => (
+        <Form>
+          <FormikControl
+            control="input"
+            type="email"
+            label="Email"
+            name="email"
+          />
+          <FormikControl
+            control="textarea"
+            label="Description"
+            name="description"
+          />
+          <FormikControl
+            control="select"
+            label="select a topic"
+            name="selectOption"
+            options={dropDownOptions}
+          />
+          <FormikControl
+            control="radio"
+            label="radio topic"
+            name="radioOption"
+            options={radioOptions   }
+          />
+          <button type="submit">Submit</button>
+        </Form>
+      )}
     </Formik>
-  )
-}
+  );
+};
 
-export default FormikContainer
+export default FormikContainer;
--- src/components/FormikControl.js
+++ src/components/FormikControl.js
@@ -1,14 +1,20 @@
 import React from "react";
 import FormikTextArea from "./FormikTextArea";
-import Input from './Input'
+import Input from "./Input";
+import RadioButton from "./RadioButton";
+import Select from "./Select";
 
 const FormikControl = (props) => {
   const { control, ...rest } = props;
   switch (control) {
-    case "input": return <Input {...rest} />
-    case "textarea": return <FormikTextArea {...rest} />
+    case "input":
+      return <Input {...rest} />;
+    case "textarea":
+      return <FormikTextArea {...rest} />;
     case "select":
+      return <Select {...rest} />;
     case "radio":
+      return <RadioButton {...rest} />
     case "checkbox":
     case "date":
     default:
