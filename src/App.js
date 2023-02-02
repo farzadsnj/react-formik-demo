3b49ba21826ba273507008465c8aeb3e8b03f861
farzadsnj <farzadsanjarani3@gmail.com>
2023-02-02 15:14:54 +0330

    cofinal

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import FormikContainer from './components/FormikContainer';
 import LogInForm from './components/LogInForm';
 import RegistrationForm from './components/RegistrationForm';
 import { theme, ThemeProvider } from '@chakra-ui/react'
-// import LoadSavedContinue from './components/LoadSavedContinue';
-// import YoutubeFormFormik from './components/YoutubeFormFormik';
-// import YoutubeFrom from './components/YoutubeFrom';
-// import YoutubeFromNew from './components/YoutubeFromNew';
+import LoadSavedContinue from './components/LoadSavedContinue';
+import YoutubeFormFormik from './components/YoutubeFormFormik';
+import YoutubeFrom from './components/YoutubeFrom';
+import YoutubeFromNew from './components/YoutubeFromNew';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        {/* <YoutubeFrom /> */}
-        {/* <YoutubeFromNew /> */}
-        {/* <YoutubeFormFormik /> */}
-        {/* <LoadSavedContinue /> */}
-        {/* <FormikContainer /> */}
+        <YoutubeFrom />
+        <YoutubeFromNew />
+        <YoutubeFormFormik />
+        <LoadSavedContinue />
+        <FormikContainer />
         <LogInForm />
-        {/* <EnrollmentForm /> */}
-        {/* <RegistrationForm /> */}
+        <EnrollmentForm />
+        <RegistrationForm />
       </div>
     </ThemeProvider>
   );
